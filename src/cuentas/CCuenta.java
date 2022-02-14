d596473817fec42b7c6111b52d8a7ddc1f34ffcc
PatriciaEloisa <patrifv1709@gmail.com>
2022-02-14 22:27:31 +0100

    añadido javadoc y comentarios en CCuenta

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,25 +1,53 @@
 package cuentas;
 
+/**
+ *
+ * @author Patif
+ */
 public class CCuenta {
 
+    /**
+     * Propiedades de la clase
+     */
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
+    /**
+     * Constructor
+     */
     public CCuenta() {
     }
 
+    /**
+     *
+     * @param nom - nombre
+     * @param cue - cuenta
+     * @param sal - saldo
+     * @param tipo - tipo de interés
+     */
     public CCuenta(String nom, String cue, double sal, double tipo) {
         nombre = nom;
         cuenta = cue;
         saldo = sal;
     }
 
+    /**
+     * Devuelve el saldo
+     *
+     * @return saldo
+     */
     public double estado() {
         return getSaldo();
     }
 
+    /**
+     * Método para ingresar dinero
+     *
+     * @param cantidad - cantidad de dinero a ingresar
+     * @throws Exception cantidad negativa
+     */
     public void ingresar(double cantidad) throws Exception {
         if (cantidad < 0) {
             throw new Exception("No se puede ingresar una cantidad negativa");
@@ -27,6 +55,12 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * Método para retirar dinero
+     *
+     * @param cantidad - cantidad de dinero a retirar
+     * @throws Exception no hay saldo o cantidad negativa
+     */
     public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0) {
             throw new Exception("No se puede retirar una cantidad negativa");
